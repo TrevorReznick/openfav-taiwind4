9e88c29ba30679f937ada3ad48852fe20c2459b8
joseph.k <double.facessss@gmail.com>
2025-02-23 08:13:55 +0100

    OPF-56 init

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
+export default defineConfig({
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    react(),
+    sentry(),
+    spotlightjs(),
+  ],
+});
